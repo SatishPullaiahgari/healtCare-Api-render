47ab2b43942659f80beda09850a963b80912227a
SatishPullaiahgari <pullaiahgarisatish456@gmail.com>
2025-04-22 21:58:38 +0530

    Install bcryptjs and its types

--- dist/server.js
+++ dist/server.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("../src/app"));
+const app_1 = __importDefault(require("./app"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config(); // Load .env variables
 const PORT = process.env.PORT || 3000;
